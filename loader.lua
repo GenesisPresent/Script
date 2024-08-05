df9f35ad890a47c9657613915fb3fd42dc6e015d
GenesisPresent <37956227+GenesisPresent@users.noreply.github.com>
2024-08-05 21:37:47 +0700

    Add files via upload

---
+++ loader.lua
@@ -0,0 +1,25 @@
+local GameList = game:HttpGet("https://pst.innomi.net/paste/dtmepweufayzftekmf2vog33/raw")
+ 
+local HttpService = game:GetService("HttpService")
+function GetGame()
+    local GameList = HttpService:JSONDecode(GameList)
+    if GameList[tostring(game.PlaceId)] then
+        return GameList[tostring(game.PlaceId)]
+    else
+        return false
+    end
+end
+ 
+local GameInfo = GetGame()
+local Script
+local GameName
+if GameInfo then
+Scriptf = game:HttpGet(GameInfo.ScriptLink)
+GameName = GameInfo.GameName
+ 
+print(GameName)   
+
+loadstring(Scriptf)()
+elseif GameInfo == false then
+    print("Script > Game Not Support ✓")
+end
