6211edeab57c8e64bec2a67deaa5415126d35120
GenesisPresent <37956227+GenesisPresent@users.noreply.github.com>
2024-08-12 07:23:40 +0700

    Update WTpressure.lua

--- WTpressure.lua
+++ WTpressure.lua
@@ -1,4 +1,28 @@
 -- src leaked by 25ms
+local ScreenGui = Instance.new("ScreenGui")
+ScreenGui.Name = "ScreenGui"
+ScreenGui.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
+ScreenGui.ResetOnSpawn = true
+ 
+local Toggle = Instance.new("TextButton")
+Toggle.Name = "Toggle"
+Toggle.Parent = ScreenGui
+Toggle.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
+Toggle.Position = UDim2.new(0.15, 0.15)
+Toggle.Size = UDim2.new(0.03, 20, 0.03, 20)
+Toggle.Font = Enum.Font.SourceSans
+Toggle.Text = "UI"
+Toggle.TextColor3 = Color3.fromRGB(248, 248, 248)
+Toggle.TextSize = 35.000
+Toggle.Draggable = true
+Toggle.MouseButton1Click:connect(function()
+    Library:ToggleUI()
+end)
+ 
+local Corner = Instance.new("UICorner")
+Corner.Name = "Corner"
+Corner.Parent = Toggle
+
 
 local Fluent = loadstring(game:HttpGet("https://github.com/dawid-scripts/Fluent/releases/latest/download/main.lua"))()
 local SaveManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/SaveManager.lua"))()
@@ -36,31 +60,6 @@ local monsterLocker = {}
 local squiddles = {}
 local render = false
 local tempDoor = nil
-
-local ScreenGui = Instance.new("ScreenGui")
-ScreenGui.Name = "ScreenGui"
-ScreenGui.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
-ScreenGui.ResetOnSpawn = true
- 
-local Toggle = Instance.new("TextButton")
-Toggle.Name = "Toggle"
-Toggle.Parent = ScreenGui
-Toggle.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
-Toggle.Position = UDim2.new(0.15, 0.15)
-Toggle.Size = UDim2.new(0.03, 20, 0.03, 20)
-Toggle.Font = Enum.Font.SourceSans
-Toggle.Text = "UI"
-Toggle.TextColor3 = Color3.fromRGB(248, 248, 248)
-Toggle.TextSize = 35.000
-Toggle.Draggable = true
-Toggle.MouseButton1Click:connect(function()
-    Library:ToggleUI()
-end)
- 
-local Corner = Instance.new("UICorner")
-Corner.Name = "Corner"
-Corner.Parent = Toggle
-
 
 local Window = Fluent:CreateWindow({
     Title = "WT | Pressure",
