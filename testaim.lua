fdcec6e0bcf36d2306dcf04af4c8cb88332bd8ad
GenesisPresent <37956227+GenesisPresent@users.noreply.github.com>
2024-08-13 14:47:54 +0700

    Add files via upload

--- testaim.lua
+++ testaim.lua
@@ -172,6 +172,14 @@ end
 -- tab 1
 
 tab1.newToggle("Enable Aimbot", "Toggle!", false, function(Value)
+    aimbotEnabled = Value
+		if aimbotEnabled then
+			aimAtTarget()
+		end
+end)
+
+
+tab1.newToggle("Enable ESP", "Toggle!", false, function(Value)
     espEnabled = Value
 		if espEnabled then
 			createESP()
