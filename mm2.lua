fac9eef0496c06c033a1703deeccd0036e389786
GenesisPresent <37956227+GenesisPresent@users.noreply.github.com>
2024-08-13 18:51:31 +0700

    Add files via upload

---
+++ mm2.lua
@@ -0,0 +1,93 @@
+local DevHubLibrary = loadstring(game:HttpGet("https://raw.githubusercontent.com/GenesisPresent/ui/main/DevHub.lua"))()
+local window = DevHubLibrary:Load("DevHub", "Default")
+local tab1 = DevHubLibrary.newTab("Main", "rbxassetid://18705759640")
+
+-- Variables
+
+
+-- Functions
+-- Start updating the Murderer and Sheriff information
+coroutine.wrap(updateRolesInfo)()
+
+local ESPFolder = Instance.new("Folder")
+ESPFolder.Name = "ESP Holder"
+ESPFolder.Parent = game.CoreGui
+
+local function AddBillboard(player)
+    local Billboard = Instance.new("BillboardGui")
+    Billboard.Name = player.Name .. "Billboard"
+    Billboard.AlwaysOnTop = true
+    Billboard.Size = UDim2.new(0, 200, 0, 50)
+    Billboard.ExtentsOffset = Vector3.new(0, 3, 0)
+    Billboard.Enabled = false
+    Billboard.Parent = ESPFolder
+
+    local TextLabel = Instance.new("TextLabel")
+    TextLabel.TextSize = 20
+    TextLabel.Text = player.Name
+    TextLabel.Font = Enum.Font.FredokaOne
+    TextLabel.BackgroundTransparency = 1
+    TextLabel.Size = UDim2.new(1, 0, 1, 0)
+    TextLabel.TextStrokeTransparency = 0
+    TextLabel.TextStrokeColor3 = Color3.new(0, 0, 0)
+    TextLabel.Parent = Billboard
+
+    repeat
+        wait()
+        pcall(function()
+            Billboard.Adornee = player.Character.Head
+            if player.Character:FindFirstChild("Knife") or player.Backpack:FindFirstChild("Knife") then
+                TextLabel.TextColor3 = Color3.new(1, 0, 0)
+                if getgenv().MurderEsp then
+                    Billboard.Enabled = true
+                end
+            elseif player.Character:FindFirstChild("Gun") or player.Backpack:FindFirstChild("Gun") then
+                TextLabel.TextColor3 = Color3.new(0, 0, 1)
+                if getgenv().SheriffEsp then
+                    Billboard.Enabled = true
+                end
+            else
+                TextLabel.TextColor3 = Color3.new(0, 1, 0)
+                if getgenv().AllEsp then
+                    Billboard.Enabled = true
+                end
+            end
+        end)
+    until not player.Parent
+end
+
+for _, player in pairs(game.Players:GetPlayers()) do
+    if player ~= game.Players.LocalPlayer then
+        coroutine.wrap(AddBillboard)(player)
+    end
+end
+
+game.Players.PlayerAdded:Connect(function(player)
+    if player ~= game.Players.LocalPlayer then
+        coroutine.wrap(AddBillboard)(player)
+    end
+end)
+
+game.Players.PlayerRemoving:Connect(function(player)
+    local billboard = ESPFolder:FindFirstChild(player.Name .. "Billboard")
+    if billboard then
+        billboard:Destroy()
+    end
+end)
+
+
+-- tab 1
+
+tab1.newToggle("DoorESP", "Toggle! Default => Humanoid", false, function(stateesp)
+    	getgenv().MurderEsp = stateesp
+        for _, billboard in ipairs(ESPFolder:GetChildren()) do
+            if billboard:IsA("BillboardGui") then
+                local playerName = billboard.Name:sub(1, -10)
+                local player = game.Players:FindFirstChild(playerName)
+                if player and (player.Character:FindFirstChild("Knife") or player.Backpack:FindFirstChild("Knife")) then
+                    billboard.Enabled = stateesp
+                end
+            end
+        end
+end)
+
